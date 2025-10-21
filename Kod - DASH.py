ca20e5086d3d4d3d89b73c74c58474fbf661c0fe
sebastian-dziadko <sebastian-dziadko@wp.pl>
2025-10-21 21:01:15 +0200

    Update Kod - DASH.py

--- Kod - DASH.py
+++ Kod - DASH.py
@@ -6,7 +6,7 @@ import pandas as pd
 import numpy as np
 
 # Wczytanie danych z Excela
-df = pd.read_excel(r"C:\Users\48690\Downloads\OECD.xlsx") #Proszę podać własną ścieżkę pliku z danymi OECD 
+df = pd.read_excel("OECD.xlsx") #Proszę podać własną ścieżkę pliku z danymi OECD 
 
 # Obliczenie średniej długości życia
 df['Średnia długość życia'] = (df['Długość życia kobiet'] + df['Długość życia mężczyzn']) / 2
@@ -160,4 +160,5 @@ def update_wykres_wiek_emerytalny(wybrane_kraje):
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 #http://127.0.0.1:8050/ lokalny link
